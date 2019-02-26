5b0458fc0543736e9c75a44cbf7de4b45c56f2ea
Ayana Bando <ayanaflax@yahoo.com>
2019-02-26 17:39:08 +0000

    attempt to make app function. add environment files

--- addition.rb
+++ addition.rb
@@ -1,9 +1,21 @@
-addition = "1+1"
-a = addition.to_i
-puts addition
-input = gets.strip
-if input.valid?
-puts "great job"
-else
-  puts "try again"
+def call
+  add
+end
+
+def add
+  addition = "1 + 1"
+  a = addition
+  puts addition
+  input = gets.strip.to_i
+  input.valid
+end
+
+def valid
+  input = gets.strip.to_i
+  answer = 2
+  if input == answer
+  puts "great job"
+  else
+    puts "try again"
+  end
 end
