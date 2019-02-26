3502a007f9082cb3beadea9ec96fd6670bee95fd
Ayana Bando <ayanaflax@yahoo.com>
2019-02-26 16:55:32 +0000

    add additional methods to models class

--- models/additions.rb
+++ models/additions.rb
@@ -1,5 +1,14 @@
 class Additions
-  def valid?
+
+  def add
+    addition = "1 + 1"
+    a = addition
+    puts addition
+    input = gets.strip.to_i
+    input.valid
+  end
+
+  def valid
     input = gets.strip.to_i
     answer = 2
     if input == answer
